b3af50327b4359012d2d2a49c90ea0cf6d990a22
Khayer Khan <coderboy1010@gmail.com>
2019-02-10 12:53:26 -0800

    Adding problem no DCP_273

---
+++ DCP_273/DCP_273/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DCP_273
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string s;
+            n = int.Parse(Console.ReadLine());
+            for(int i=0; i<n; i++)
+            {
+                s = Console.ReadLine();
+                //var k = s.Split(' ');
+                //var v = Convert.ToString(k[0]);
+                int n1 = s.Length;
+                // Console.WriteLine(n1);
+                int y = 0;
+                for(int j=0; j<n1; j++)
+                {
+
+                    if (s[j] == 'r')
+                    {
+                        y++;
+                        break;
+                    }
+                }
+                
+                if (y == 1)
+                {
+                    Console.WriteLine("Not vely easy");
+
+                }
+                else
+                {
+                    Console.WriteLine("Vely easy");
+                }
+
+            }
+        }
+    }
+}
